2edbd59d97c198a99358afe1e41ced7235d48987
Srinjoy <srinjoypramanik2004@gmail.com>
2025-03-01 22:40:36 +0530

    done til video 4

--- hello.js
+++ hello.js
@@ -1,2 +1,6 @@
-console.log("hello world")
-console.log(window)
+console.log("hello world");
+//console.log(window)
+const math = require("./math"); //  ./ stands for to import modue from current directory or else it means to import in built module
+
+console.log("math value is",math.sub(10,4))
+
